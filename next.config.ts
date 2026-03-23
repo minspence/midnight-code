bf523ef7f24b87f34df3165a953a087359b769ce
seigiakatsuka <minuitspence@gmail.com>
2026-03-23 12:13:28 -0400

    Updated next.config.ts for output for GitHub pages, and added nojekyll file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: "export",
+    basePath: "/midnight-code",
+    assetPrefix: "/midnight-code/",
 };
+module.exports = nextConfig;
 
 export default nextConfig;
